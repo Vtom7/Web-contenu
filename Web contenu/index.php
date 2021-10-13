5d567251c9f838fb5c5e8c924d746ed9ab42469f
Vtom7 <70704273+Vtom7@users.noreply.github.com>
2021-10-13 10:55:15 +0200

    Add files via upload

---
+++ Web contenu/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Fonction Main robotisée</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css" />
+		
+	</head>
+	<body>
+		<h3>VALLET Thomas</h3>
+		<h3>TRAORE Simon</h3>
+		<h3>TS2SNIR</h3>
+		<center>
+		<h1><FONT size="50pt"><u>Fonction Main robotisée</u></FONT></h1>
+		</center>
+		<p>
+		<center>
+		<img class="fit-picture" src="header-812271.jpg" alt="header-812271" style="width:1300px; height:270px;">
+		</center>
+		</p>
+		<center>
+		<p><FONT face="arial">En robotique, la main est l'une des parties les plus complexes à maîtriser. Ses chercheurs ont conçu une main robotisée à cinq doigts qui fait montre d'une grande adresse. Surtout, grâce à une intelligence artificielle (IA), elle est capable d'améliorer sa dextérité sans intervention humaine.</FONT></p>
+		<p><FONT face="arial">La main robotisée a la possibilité de faire des signes sous forme de lettres de l'alphabet. On l'appelle le langage des signes. Voici plusieurs signes :</FONT></p>
+		</center>
+		<center>
+		<table>
+			<tr><th><img class="fit-picture" src="Capture 1.PNG" alt="Capture 1"><input class="favorite styled" type="button" value="A"></th><th><img class="fit-picture" src="Capture 2.PNG" alt="Capture 2"><input class="favorite styled" type="button" value="B"></th>
+			<th><img class="fit-picture" src="Capture 3.PNG" alt="Capture 3"><input class="favorite styled" type="button" value="C"></th><th><img class="fit-picture" src="Capture 4.PNG" alt="Capture 4"><input class="favorite styled" type="button" value="D"></th>
+			<th><img class="fit-picture" src="Capture 5.PNG" alt="Capture 5"><input class="favorite styled" type="button" value="E"></th><th><img class="fit-picture" src="Capture 6.PNG" alt="Capture 6"><input class="favorite styled" type="button" value="F"></th>
+			<th><img class="fit-picture" src="Capture 7.PNG" alt="Capture 7"><input class="favorite styled" type="button" value="G"></th><th><img class="fit-picture" src="Capture 8.PNG" alt="Capture 8"><input class="favorite styled" type="button" value="H"></th>
+			<th><img class="fit-picture" src="Capture 9.PNG" alt="Capture 9"><input class="favorite styled" type="button" value="I"></th><th><img class="fit-picture" src="Capture 10.PNG" alt="Capture 10"><input class="favorite styled" type="button" value="J"></th>
+			<th><img class="fit-picture" src="Capture 11.PNG" alt="Capture 11"><input class="favorite styled" type="button" value="K"></th><th><img class="fit-picture" src="Capture 12.PNG" alt="Capture 12"><input class="favorite styled" type="button" value="L"></th>
+			<th><img class="fit-picture" src="Capture 13.PNG" alt="Capture 13"><input class="favorite styled" type="button" value="M"></th><th><img class="fit-picture" src="Capture 14.PNG" alt="Capture 14"><input class="favorite styled" type="button" value="N"></th>
+			<th><img class="fit-picture" src="Capture 15.PNG" alt="Capture 15"><input class="favorite styled" type="button" value="O"></th><th><img class="fit-picture" src="Capture 16.PNG" alt="Capture 16"><input class="favorite styled" type="button" value="P"></th>
+			<th><img class="fit-picture" src="Capture 17.PNG" alt="Capture 17"><input class="favorite styled" type="button" value="Q"></th><th><img class="fit-picture" src="Capture 18.PNG" alt="Capture 18"><input class="favorite styled" type="button" value="R"></th>
+			<th><img class="fit-picture" src="Capture 19.PNG" alt="Capture 19"><input class="favorite styled" type="button" value="S"></th><th><img class="fit-picture" src="Capture 20.PNG" alt="Capture 20"><input class="favorite styled" type="button" value="T"></th>
+			<th><img class="fit-picture" src="Capture 21.PNG" alt="Capture 21"><input class="favorite styled" type="button" value="U"></th><th><img class="fit-picture" src="Capture 22.PNG" alt="Capture 22"><input class="favorite styled" type="button" value="V"></th>
+			<th><img class="fit-picture" src="Capture 23.PNG" alt="Capture 23"><input class="favorite styled" type="button" value="W"></th><th><img class="fit-picture" src="Capture 24.PNG" alt="Capture 24"><input class="favorite styled" type="button" value="X"></th>
+			<th><img class="fit-picture" src="Capture 25.PNG" alt="Capture 25"><input class="favorite styled" type="button" value="Y"></th><th><img class="fit-picture" src="Capture 26.PNG" alt="Capture 26"><input class="favorite styled" type="button" value="Z"></th></tr>
+			<tr><td>Pouce</td>
+			<td>Index Majeur Annulaire Auriculaire</td>
+			<td>Pouce Index Majeur Annulaire Auriculaire</td>
+			<td>Pouce Majeur Annulaire Auriculaire</td>
+			<td>Aucuns</td>
+			<td>(Pouce) (Index) Majeur Annulaire Auriculaire</td>
+			<td>(Pouce) Index</td>
+			<td>Index Auriculaire</td>
+			<td>Auriculaire</td>
+			<td>Pouce Auriculaire</td>
+			<td>Pouce Index Majeur</td>
+			<td>Pouce Index</td>
+			<td>Index Majeur Annulaire Auriculaire</td>
+			<td>Index Majeur</td>
+			<td>Pouce Index Majeur Annulaire Auriculaire</td>
+			<td>(Pouce) Index Majeur</td>
+			<td>(Pouce) Index</td>
+			<td>Index Majeur</td>
+			<td>(Pouce)</td>
+			<td>(Pouce) (Index) Majeur Annulaire Auriculaire</td>
+			<td>Index Majeur</td>
+			<td>Index Majeur</td>
+			<td>Index Majeur Annulaire</td>
+			<td>(Pouce) Index Majeur</td>
+			<td>Pouce Auriculaire</td>
+			<td>Index</td></tr>
+		</table>
+		</center>
+	</body>
+</html>
